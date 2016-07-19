d9c5eedeac6eb2c490a91a302c46a792acc175bb
Sanjay <saggarwal@gmail.com>
2016-07-19 14:55:26 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/saaggarw/firstandroid/FirstExampleActivity.java
@@ -0,0 +1,13 @@
+package com.example.saaggarw.firstandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_example);
+    }
+}
